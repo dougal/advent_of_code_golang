1fa84f8b49c2ff2588bda555c67fdcd1c1dcbc20
Douglas F Shearer <dougal.s@gmail.com>
2023-07-06 21:25:30 +0100

    Simpler approach with fixed variables and bubble sorting.

--- 01/part_2/main.go
+++ 01/part_2/main.go
@@ -10,52 +10,37 @@ import (
 
 // Usage:
 func main() {
-	fmt.Println(most_calories_by_three_elves(os.Stdin))
+	fmt.Println(most_calories_by_three_elves(os.Stdin)) // 197,400
 }
 
-type TopThreeElves []int64
-
-// Take a calorie count, add to array if larger than any of the existing value.
-func (t TopThreeElves) CompareAndAddElf(c int64) {
-	for i, v := range t {
-		if c > v {
-			t[i] = c
-			break
-		}
-	}
-}
-
-func InitTopThreeElves() TopThreeElves {
-	return make([]int64, 3)
-}
-
-func (t TopThreeElves) Sum() int64 {
-	return t[0] + t[1] + t[2]
-}
-
-func most_calories_by_three_elves(r io.Reader) int64 {
+func most_calories_by_three_elves(r io.Reader) int {
 	scanner := bufio.NewScanner(r)
 	scanner.Split(bufio.ScanLines)
 
-	var topThreeCalorieCounts = InitTopThreeElves()
-	var currentCalorieCount int64 = 0
+	maxCalories0 := 0
+	maxCalories1 := 0
+	maxCalories2 := 0
+	currentCalorieCount := 0
 
 	for scanner.Scan() {
-		line := scanner.Text()
+		c, err := strconv.Atoi(scanner.Text())
+		currentCalorieCount += c
 
-		if line == "" {
-			topThreeCalorieCounts.CompareAndAddElf(currentCalorieCount)
-			fmt.Println(topThreeCalorieCounts)
+		if err != nil { // Empty line
+			if currentCalorieCount > maxCalories0 {
+				maxCalories0 = currentCalorieCount
+			}
+			// Low budget bubble sort
+			if maxCalories0 > maxCalories1 {
+				maxCalories0, maxCalories1 = maxCalories1, maxCalories0
+			}
+			// And again.
+			if maxCalories1 > maxCalories2 {
+				maxCalories1, maxCalories2 = maxCalories2, maxCalories1
+			}
 			currentCalorieCount = 0
-			continue
-		}
-
-		if c, err := strconv.ParseInt(line, 10, 64); err == nil {
-			currentCalorieCount += c
 		}
 	}
 
-	topThreeCalorieCounts.CompareAndAddElf(currentCalorieCount)
-
-	return topThreeCalorieCounts.Sum()
+	return maxCalories0 + maxCalories1 + maxCalories2
 }
