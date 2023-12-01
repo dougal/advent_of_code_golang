3be6ba1a899a281507f6a08bcc41379d2e6e6339
Douglas F Shearer <dougal.s@gmail.com>
2023-12-01 15:45:41 +0000

    Day 1 part b

---
+++ 2023/day_01/part_b.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+  fmt.Println(summation())
+}
+
+var wordNumbers = map[string]int{
+  "one":   1,
+  "two":   2,
+  "three": 3,
+  "four":  4,
+  "five":  5,
+  "six":   6,
+  "seven": 7,
+  "eight": 8,
+  "nine":  9,
+  "1":     1,
+  "2":     2,
+  "3":     3,
+  "4":     4,
+  "5":     5,
+  "6":     6,
+  "7":     7,
+  "8":     8,
+  "9":     9,
+}
+
+// Process each line searching for all the number.
+// Word-numbers may overlap, example: nineight is 9 and 8.
+// Take the first and last digit.
+// String concat these digits to form a two digit number.
+// Sum the numbers from every line.
+func summation() int {
+  f, err := os.Open("input_a.txt")
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  sum := 0
+  scanner := bufio.NewScanner(f)
+  scanner.Split(bufio.ScanLines)
+
+  for scanner.Scan() {
+    line := scanner.Text()
+    digits := []int{}
+
+    for i := 0; i < len(line); i++ {
+      haystack := line[i:]
+      for needle, n := range wordNumbers {
+	if strings.HasPrefix(haystack, needle) {
+	  digits = append(digits, n)
+	  break
+	}
+      }
+    }
+
+    sum += digits[0] * 10 + digits[len(digits) - 1]
+  }
+
+  return sum
+}
