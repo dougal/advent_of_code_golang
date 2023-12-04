9e57cf2fa07969fdf0976a1197ae7e4fe1184456
Douglas F Shearer <dougal.s@gmail.com>
2023-12-04 11:38:35 +0000

    Add more boilerplate to day init.

--- 2023/init.sh
+++ 2023/init.sh
@@ -4,32 +4,88 @@ dir=$1
 
 mkdir "$dir"
 
+touch "$dir/input.txt"
+
+# Part A
+########
 mkdir "$dir/part_a"
 cd "$dir/part_a"
 go mod init dougal/advent_of_code_golang/2023/part_a
-cd -
 echo "package main
 
+import (
+  \"fmt\"
+  \"io\"
+  \"log\"
+  \"os\"
+)
+
 func main() {
+  f, err := os.Open(\"../input.txt\")
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  fmt.Println(doSomething(f))
 }
-" > "$dir/part_a/main.go"
+
+func doSomething(input io.Reader) int {
+  return 123
+}
+" > "main.go"
+
 echo "package main
+
+import (
+  \"testing\"
+)
 
 // func TestSomething(t *testing.T) {
 // }
-" > "$dir/part_a/main_test.go"
+" > "main_test.go"
 
+go fmt .
+
+cd -
+
+# Part B
+########
 mkdir "$dir/part_b"
 cd "$dir/part_b"
 go mod init dougal/advent_of_code_golang/2023/part_b
-cd -
 echo "package main
 
+import (
+  \"fmt\"
+  \"io\"
+  \"log\"
+  \"os\"
+)
+
 func main() {
+  f, err := os.Open(\"../input.txt\")
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  fmt.Println(doSomething(f))
 }
-" > "$dir/part_b/main.go"
+
+func doSomething(input io.Reader) int {
+  return 123
+}
+" > "main.go"
+
 echo "package main
+
+import (
+  \"testing\"
+)
 
 // func TestSomething(t *testing.T) {
 // }
-" > "$dir/part_b/main_test.go"
+" > "main_test.go"
+
+go fmt .
+
+cd -
