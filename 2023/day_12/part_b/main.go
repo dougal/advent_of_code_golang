b3390ab09586bc4528d6e4568cd59558920cf6d9
Douglas F Shearer <dougal.s@gmail.com>
2023-12-12 19:18:29 +0000

    Recursion from part a.
    Still too slow to be practical.

--- 2023/day_12/part_b/main.go
+++ 2023/day_12/part_b/main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -24,12 +25,12 @@ func sumArrangements(input io.Reader) int {
 	scanner.Split(bufio.ScanLines)
 
 	c := 0
+	l := 0
 
-	// var l int
 	for scanner.Scan() {
-		// fmt.Println(l)
 		c += arrangements(scanner.Text())
-		// l++
+		l++
+		fmt.Println(l)
 	}
 
 	return c
@@ -39,152 +40,113 @@ func arrangements(s string) int {
 	var c int
 	line := NewLine(s)
 
-	// permutations := int(math.Pow(float64(2), float64(line.unknownCount)))
-	permutations := maxPermutation(line.unknownCount, line.brokenCount)
-	fmt.Println(permutations)
-Outer:
-	for i := 0; i < permutations; i++ {
-		arrangement := make([]string, line.unknownCount)
+	c += replaceNextUnknown(line, "#")
+	c += replaceNextUnknown(line, ".")
 
-		var (
-			n           = i
-			j           int
-			brokenAdded int
-		)
+	return c
+}
 
-		for n > 0 {
-			if n%2 == 0 {
-				arrangement[j] = "."
-			} else {
-				arrangement[j] = "#"
-				brokenAdded++
-				// fmt.Printf("%d > %d\n", brokenAdded, line.unknownBrokenCount)
-				if brokenAdded > line.unknownBrokenCount {
-					continue Outer
-				}
-			}
+func replaceNextUnknown(l Line, r string) int {
+	before, after, _ := strings.Cut(l.cells, "?")
 
-			n = n / 2
-			j++
+	l.cells = before + r + after
+
+	if strings.Index(l.cells, "?") == -1 {
+		if l.validPrefix() {
+			return 1
+		} else {
+			return 0
 		}
+	}
 
-		// fmt.Println(arrangement)
-		if line.satisfiedBy(arrangement) {
-			c++
-		}
+	var c int
+	if l.validPrefix() {
+		c += replaceNextUnknown(l, "#")
+		c += replaceNextUnknown(l, ".")
 	}
 
 	return c
 }
 
 type Line struct {
-	cells              []string
-	grouping           []int
-	unknownCount       int
-	brokenCount        int
-	knownBrokenCount   int
-	unknownBrokenCount int
+	cells    string
+	grouping []int
+	brokenCount int
+	workingCount int
 }
 
 func NewLine(s string) Line {
 	var l Line
 	sc, gs, _ := strings.Cut(s, " ")
 
-	cellGroup := strings.Split(sc, "")
 	for i := 0; i < 5; i++ {
-		l.cells = append(l.cells, cellGroup...)
-		l.cells = append(l.cells, "?")
+		if i > 0 {
+			l.cells += "?"
+		}
+		l.cells += sc
 	}
 
-	var gg []int
-	for _, g := range strings.Split(gs, ",") {
-		gi, _ := strconv.Atoi(g)
-		gg = append(gg, gi)
-	}
 	for i := 0; i < 5; i++ {
-		l.grouping = append(l.grouping, gg...)
-	}
-
-	for _, c := range l.grouping {
-		l.brokenCount += c
-	}
-
-	for _, c := range l.cells {
-		if c == "?" {
-			l.unknownCount++
-		} else if c == "#" {
-			l.knownBrokenCount++
+		// Split goes brrrrrrr 5 times
+		for _, g := range strings.Split(gs, ",") {
+			gi, _ := strconv.Atoi(g)
+			l.grouping = append(l.grouping, gi)
+			l.brokenCount+= gi
 		}
 	}
 
-	l.unknownBrokenCount = l.brokenCount - l.knownBrokenCount
-
-	// fmt.Println(l.cells)
-	// fmt.Println(l.grouping)
-	// fmt.Println(l.brokenCount)
-	// fmt.Println(l.knownBrokenCount)
-	// fmt.Println(l.unknownBrokenCount)
+	l.workingCount = len(l.cells) - l.brokenCount
 
 	return l
 }
 
-// return true if the arrangement, when applied, satisfies the grouping
-func (l Line) satisfiedBy(arrangement []string) bool {
-	// Never satisfied if not enough broken wells
-	var brokenCount int
-	for _, c := range arrangement {
-		if c == "#" {
-			brokenCount++
+// Returns false if no extend prefix can satisfy the groupings.
+// Returns true if:
+// - There are no groupings in the prefix
+// - Any groupings in the prefix partially satisfy the groupings
+// TODO: Forward-checking there are enough ? left to insert enough # or . to satisfy.
+func (l Line) validPrefix() bool {
+	prefix, _, _ := strings.Cut(l.cells, "?")
+	var groups []int
+	var curGroup int
+	for _, c := range prefix {
+		if c == '.' {
+			if curGroup > 0 {
+				groups = append(groups, curGroup)
+				curGroup = 0
+			}
+		} else if c == '#' {
+			curGroup++
 		}
 	}
 
-	var missingIndex int
-	var cells []string
-
-	for _, c := range l.cells {
-		if c != "?" {
-			cells = append(cells, c)
-			continue
-		}
-
-		cells = append(cells, arrangement[missingIndex])
-		missingIndex++
+	if curGroup > 0 {
+		groups = append(groups, curGroup)
+		curGroup = 0
 	}
 
-	s := strings.Join(cells, "")
+	// Too many groups
+	if len(groups) > len(l.grouping) {
+		return false
+	}
 
-	gs := strings.Split(s, ".")
+	for i, g := range groups {
+		isLast := i == len(groups)-1
 
-	var gc int
-	for _, g := range gs {
-		if g == "" {
-			continue
+		if isLast {
+			if g > l.grouping[i] {
+				return false
+			}
+		} else {
+			if g != l.grouping[i] {
+				return false
+			}
 		}
+	}
 
-		if len(g) != l.grouping[gc] {
-			return false
-		}
-		gc++
+	if strings.Index(l.cells, "?") == -1 {
+		return slices.Equal(groups, l.grouping)
 	}
 
 	return true
-}
-
-func maxPermutation(unknownCount, brokenCount int) int {
-	// If 6 unknown, and 3 broken:
-	// ...###
-
-	var s int
-
-	m := 1
-	for i := 0; i < unknownCount; i++ {
-		if i < unknownCount-brokenCount {
-			continue
-		}
-
-		s += m
-		m *= 2
-	}
-
-	return s
 }
