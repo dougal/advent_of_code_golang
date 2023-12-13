6af4fd76e951332a256ea0001040afc5c200a707
Douglas F Shearer <dougal.s@gmail.com>
2023-12-13 22:57:44 +0000

    Split corrected and uncorrected reflection finding.

--- 2023/day_13/part_b/main.go
+++ 2023/day_13/part_b/main.go
@@ -23,11 +23,11 @@ func sumReflections(input io.Reader) int {
 	s := 0
 
 	for _, p := range puzzles {
-		origH:= horizontalIndexOfReflection(p, false)
-		origV:= verticalIndexOfReflection(p, false)
+		origH := horizontalIndexOfReflection(p)
+		origV := verticalIndexOfReflection(p)
 
-		newH := horizontalIndexOfReflection(p, true)
-		newV := verticalIndexOfReflection(p, true)
+		newH := correctedHorizontalIndexOfReflection(p)
+		newV := correctedVerticalIndexOfReflection(p)
 
 		// Only add the horizontal value if it is changed by the smudge.
 		if origH != newH {
@@ -43,7 +43,7 @@ func sumReflections(input io.Reader) int {
 	return s
 }
 
-func verticalIndexOfReflection(p [][]rune, errorCorrect bool) int {
+func verticalIndexOfReflection(p [][]rune) int {
 	// Rotate
 	p2 := make([][]rune, len(p[0]))
 	// for range p[0] {
@@ -56,10 +56,55 @@ func verticalIndexOfReflection(p [][]rune, errorCorrect bool) int {
 		}
 	}
 
-	return horizontalIndexOfReflection(p2, errorCorrect)
+	return horizontalIndexOfReflection(p2)
 }
 
-func horizontalIndexOfReflection(p [][]rune, errorCorrect bool) int {
+func correctedVerticalIndexOfReflection(p [][]rune) int {
+	// Rotate
+	p2 := make([][]rune, len(p[0]))
+	// for range p[0] {
+	// p2 = append(p2, make([]rune))
+	// }
+
+	for _, row := range p {
+		for j, c := range row {
+			p2[j] = append(p2[j], c)
+		}
+	}
+
+	return correctedHorizontalIndexOfReflection(p2)
+}
+
+func horizontalIndexOfReflection(p [][]rune) int {
+	// Find difference of 1 between two lines.
+	// Flip the different position
+	// Test for reflection
+Outer:
+	for i := 1; i < len(p); i++ {
+		top := p[0:i]
+		bottom := p[i:]
+
+		var max int
+		if len(top) > len(bottom) {
+			max = len(bottom)
+		} else {
+			max = len(top)
+		}
+
+		for j := 0; j < max; j++ {
+			equal := slices.Equal(top[len(top)-1-j], bottom[j])
+			if !equal {
+				continue Outer
+			}
+		}
+
+		return i
+
+	}
+	return 0
+}
+
+func correctedHorizontalIndexOfReflection(p [][]rune) int {
 	// Find difference of 1 between two lines.
 	// Flip the different position
 	// Test for reflection
@@ -79,11 +124,7 @@ Outer:
 		for j := 0; j < max; j++ {
 			var s bool
 			var equal bool
-			if errorCorrect {
-				equal, s = equalIfOneFlipped(top[len(top)-1-j], bottom[j])
-			} else {
-				equal = slices.Equal(top[len(top)-1-j], bottom[j])
-			}
+			equal, s = equalIfOneFlipped(top[len(top)-1-j], bottom[j])
 			if !equal {
 				continue Outer
 			}
