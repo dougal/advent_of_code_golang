733f2c7ac314bcbd3c07a42f3f72069c3b37bcbc
Douglas F Shearer <dougal.s@gmail.com>
2023-12-17 20:03:46 +0000

    15 part b.
    That seemed far too easy. Test and input ran first time.

--- 2023/day_15/part_b/main.go
+++ 2023/day_15/part_b/main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -14,20 +15,113 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(SumHashes(f))
+	fmt.Println(SumFocussingPower(f))
 }
 
-func SumHashes(input io.Reader) int {
-	s := 0
+func SumFocussingPower(input io.Reader) int {
 
 	all, _ := io.ReadAll(input)
-	words := strings.Split(string(all), ",")
+	steps := strings.Split(string(all), ",")
+	boxes := make([]Box, 256)
 
-	for _, w := range words {
-		s += hash(w)
+	for _, s := range steps {
+		label, op, focalLength := parseStep(s)
+		i := hash(label)
+
+		switch op {
+		case "-":
+			boxes[i] = boxes[i].RemoveLens(label)
+		case "=":
+			boxes[i] = boxes[i].AddLens(label, focalLength)
+		}
+	}
+
+	s := 0
+	for i, b := range boxes {
+		s += b.SumFocussingPowers(i + 1)
+	}
+	return s
+}
+
+type Lens struct {
+	label       string
+	focalLength int
+}
+
+type Box []Lens
+
+func (b Box) SumFocussingPowers(boxNum int) int {
+	var s int
+
+	for i, lens := range b {
+		s += boxNum * (i + 1) * lens.focalLength
 	}
 
 	return s
+}
+
+func (b Box) AddLens(label string, focalLength int) Box {
+	lens := Lens{label, focalLength}
+
+	existingInd := -1
+	for i, l := range b {
+		if l.label == label {
+			existingInd = i
+			break
+		}
+	}
+
+	if existingInd == -1 {
+		b = append(b, lens)
+	} else {
+		b[existingInd] = lens
+	}
+
+	return b
+}
+
+func (b Box) RemoveLens(label string) Box {
+	var newBox Box
+
+	for _, lens := range b {
+		if lens.label != label {
+			newBox = append(newBox, lens)
+		}
+	}
+
+	return newBox
+}
+
+func parseStep(in string) (string, string, int) {
+	var (
+		label, op, pow string
+		offset         int
+	)
+
+	for i, c := range in {
+		if c == '-' || c == '=' {
+			op = string(c)
+			offset = i
+			break
+		}
+
+		label += string(c)
+	}
+
+	pow, _ = strings.CutSuffix(string(in[offset+1:]), "\n")
+
+	var powi int
+	var err error
+	if op == "=" {
+		powi, err = strconv.Atoi(pow)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		powi = -1
+	}
+
+	return label, op, powi
 }
 
 func hash(in string) int {
--- 2023/day_15/part_b/main_test.go
+++ 2023/day_15/part_b/main_test.go
@@ -5,17 +5,16 @@ import (
 	"testing"
 )
 
-var SumHashescases = []struct {
+var SumFocussingPowerCases = []struct {
 	input    string
 	expected int
 }{
-	{"HASH", 52},
-	{"rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7", 1320},
+	{"rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7", 145},
 }
 
 func TestSumHashes(t *testing.T) {
-	for _, c := range SumHashescases {
-		actual := SumHashes(strings.NewReader(c.input))
+	for _, c := range SumFocussingPowerCases {
+		actual := SumFocussingPower(strings.NewReader(c.input))
 		if actual != c.expected {
 			t.Errorf("Expected %d but got %d\n", c.expected, actual)
 		}
